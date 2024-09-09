137346d4c0fc2b80bf8cfb18f6812bc5304c033d
fabricioguimaraesdeoliveira <fabricio.oliveira@partner.ideen.tech>
2024-09-08 23:06:47 -0400

    Criando tabelas de order e payments no dynamo

--- modules/dynamo/dynamo.tf
+++ modules/dynamo/dynamo.tf
@@ -23,3 +23,70 @@ resource "aws_dynamodb_table" "tf_clients_table" {
     projection_type    = "ALL"
   }
 }
+
+resource "aws_dynamodb_table" "tf_order_table" {
+  name           = "tf-order-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = "5"
+  write_capacity = "5"
+  hash_key       = "codigo"
+  range_key      = "status"
+
+  attribute {
+    name = "codigo"
+    type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "status-index"
+    hash_key           = "status"
+    write_capacity     = 5
+    read_capacity      = 5
+    projection_type    = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "payments_table" {
+  name         = "payments-table"
+  billing_mode = "PROVISIONED"
+  read_capacity  = 2
+  write_capacity = 2
+  hash_key    = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "status-index"
+    hash_key        = "status"
+    write_capacity  = 2
+    read_capacity   = 2
+    projection_type = "ALL"
+
+    attribute {
+      name = "status"
+      type = "S"
+    }
+  }
+
+  global_secondary_index {
+    name            = "qrCode-index"
+    hash_key        = "qrCode"
+    write_capacity  = 2
+    read_capacity   = 2
+    projection_type = "ALL"
+
+    attribute {
+      name = "qrCode"
+      type = "S"
+    }
+  }
+}
+
--- modules/dynamo/outputs.tf
+++ modules/dynamo/outputs.tf
@@ -1,3 +1,9 @@
 output "aws_dynamodb_table_name" {
   value = aws_dynamodb_table.tf_clients_table.name
 }
+output "aws_dynamodb_table_name_order" {
+  value = aws_dynamodb_table.tf_order_table.name
+}
+output "aws_dynamodb_payments_table_name" {
+  value = aws_dynamodb_table.payments_table.name
+}
